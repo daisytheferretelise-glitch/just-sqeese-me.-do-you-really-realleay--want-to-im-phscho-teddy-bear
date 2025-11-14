aa44abe1b8018056d8544ddf3b920e01c731bca3
daisytheferretelise-glitch <daisytheferretelise@gmail.com>
2025-11-14 19:35:04 +0000

    print

---
+++ noooooooo i ate my ice cream.py
@@ -0,0 +1,8 @@
+print("grr")
+print(4,"ice creams i lost")
+print(" i lost all my ice cream \n she flushed them down the tree toilet the tree used my ice cream as fertilzer \n but now my ice cream is dead")
+# variables=value
+cream=" choclate ice cream"
+choclate="and cream"
+print(cream)
+print(choclate)
